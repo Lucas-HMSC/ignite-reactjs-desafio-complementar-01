f37d6f87d2e879946d5bec351104ed378d604378
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-12-15 21:25:56 -0300

    Content e Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,12 @@
-import { useEffect, useState } from 'react';
-
-import { MovieCard } from './components/MovieCard';
+import { useState } from 'react';
 
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
 
-import { api } from './services/api';
-
 import './styles/global.scss';
-
-import './styles/content.scss';
-import { Header } from './components/Header';
-
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
-
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
-
-  const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
-
-  useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
-      setMovies(response.data);
-    });
-
-    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
-      setSelectedGenre(response.data);
-    })
-  }, [selectedGenreId]);
 
   function handleClickButton(id: number) {
     setSelectedGenreId(id);
@@ -56,19 +19,9 @@ export function App() {
         handleClickButton={handleClickButton}
       />
 
-      <div className="container">
-        <Header 
-          selectedGenre={selectedGenre}
-        />
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content 
+        selectedGenreId={selectedGenreId}
+      />
     </div>
   )
 }
